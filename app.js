f880f86b19151ba4db20166ab0005ebe47d5d4e2
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-02-19 13:48:29 -0800

    Add EJS and functionality for such

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express';
 const app = express();
+
+// Set view engine
+app.set('view engine', 'ejs');
+
 const PORT = 3002;
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    let user = {
+        name: 'Gigi',
+        color: 'mauve',
+        food: 'pork fried rice',
+        grade: 3.8,
+        age: 44
+    }
+
+    res.render('home', { user });
 });
 
 app.listen(PORT, () => {
